31bb4841a5c642d0941243933afa12f40caefcac
Nalin <79359909+NotNalin@users.noreply.github.com>
2025-01-04 19:56:34 +0530

    update year_of_50.sh

--- 2. Shell Programming/year_of_50.sh
+++ 2. Shell Programming/year_of_50.sh
@@ -5,9 +5,9 @@ read year
 
 if [ $age -ge 50 ]
 then
-    echo "You are already 50 or older!"
+    year_50=$((year + 50 - age))
+    echo "You turned 50 in the year $year_50."
 else
-    years_to_50=$((50 - age))
-    year_of_50=$((year + years_to_50))
-    echo "You will turn 50 in the year $year_of_50."
+    year_50=$((year + 50 - age))
+    echo "You will turn 50 in the year $year_50."
 fi
